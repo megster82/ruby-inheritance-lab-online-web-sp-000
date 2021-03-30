f8330d8f24131820758904e103de2955db689fd3
meaghan jennings <meaghanljennings@gmail.com>
2021-03-30 17:19:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,10 @@ require_relative "./user.rb"
 
 class Student < User
   
-  def initialize(knowledge)
-    @knowledge = []
-  end 
+
+def learn(string_of_knowledge = "Ruby framework Rails gem bundle update")
+  @knowledge << string_of_knowledge
+end 
 
 
   
